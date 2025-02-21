606c37f53fbd2340beb9c26dc8326ded2b5839f0
Your Name <you@example.com>
2025-02-21 17:37:18 +0100

    Comentarios
    Como haria el ultimo script

---
+++ Assets/Scripts/Lakitu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Le meteria un metodo spawner que cada vez que se llegue a un tiempo maximo de la corrutina se llame el metodo, ademas, la bola que suelta tendria otro script con una "death zone" 
+    // que se llamaria a GameManager con instance porque tiene un metodo de resetear la escena
