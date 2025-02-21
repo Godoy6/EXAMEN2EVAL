1f163483d3bddc858efd27188076e96c17685044
Your Name <you@example.com>
2025-02-21 16:21:24 +0100

    Apartado 3
    Hecho enterito

--- Assets/Scripts/WitchTime.cs
+++ Assets/Scripts/WitchTime.cs
@@ -1,12 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class WitchTime : MonoBehaviour
 {
-    // Update is called once per frame
+    private bool isSlowed = false;
+
+    public AudioClip WitchTimeSound;
+
     void Update()
     {
+        if (!isSlowed && Input.GetButtonDown("Fire2")) 
+        {
+            StartCoroutine(SlowTime());
+        }
+    }
 
+    private IEnumerator SlowTime() 
+    {
+        AudioManager.instance.PlayAudio(WitchTimeSound, "WitchTimeSound");
+
+        isSlowed = true;
+        Time.timeScale = 0.25f;
+        yield return new WaitForSecondsRealtime(5.512f);
+        Time.timeScale = 1.0f;
+        isSlowed = false;
     }
 }
